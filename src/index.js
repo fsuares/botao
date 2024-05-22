b57a5ab072f83d65decbe9105e971b366084b529
Fernando Suares <fernandoaqs@outlook.com.br>
2024-05-22 18:03:50 -0300

    feat: add command handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
 
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+
+const fs = require('node:fs');
+const path = require('node:path');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, '../commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.login(TOKEN);
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error('Not Found Command');
+		return;
+	} try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply('Command error');
+	};
+});
