d8d807962e857908c98fba063fe2763e8f5d9162
Fernando Suares <fernandoaqs@outlook.com.br>
2024-05-22 12:47:01 -0300

    :tada: initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(TOKEN);
